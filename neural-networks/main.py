Neural Network: Fix normalization and implement cost computation

neural-networks/main.py
@@ -1,8 +1,9 @@
 from neural_network import Model
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
-np.random.seed(0)
+np.random.seed(3)
 
 def normalize(data, method='min-max'):
     
@@ -27,6 +28,7 @@ def normalize(data, method='min-max'):
         denominator = np.std(data, axis=0)
         return numerator/denominator
     
+
 def segregate_target(data):
 
     '''
@@ -39,14 +41,30 @@ def segregate_target(data):
     
     return X, t
 
-def train_test_validation_split(X, t, test_ratio=0.33):
+
+def train_test_validation_split(X, t, test_ratio=0.15):
 
     '''
         Make use of sklearn's `train_test_split` to split `X` into train, test and validation sets.
     '''
+
+    m = X.shape[0]
+    test_size = (int)(m*test_ratio)
+    val_size = test_size
+    train_size = m - (test_size + val_size)
+
+    perm = np.random.permutation(m)
+    X = X[perm, :]
+    t = t[perm, :]
+    X_train = X[:train_size, :]
+    t_train = t[:train_size, :]
+    X_valid = X[train_size:train_size+val_size, :]
+    t_valid = t[train_size:train_size+val_size, :]
+    X_test = X[train_size+val_size:, :]
+    t_test = t[train_size+val_size:, :]
     
-    X_train, X_test, t_train, t_test = train_test_split(X, t, test_size=test_ratio, random_state=0)
-    X_valid, X_test, t_valid, t_test = train_test_split(X_test, t_test, test_size=0.5, random_state=0)
+    # X_train, X_test, t_train, t_test = train_test_split(X, t, test_size=test_ratio, random_state=0)
+    # X_valid, X_test, t_valid, t_test = train_test_split(X_test, t_test, test_size=0.5, random_state=0)
     
     data = {
         'X_train': X_train,
@@ -59,6 +77,7 @@ def train_test_validation_split(X, t, test_ratio=0.33):
     
     return data
 
+
 def load_data(path):
 
     '''
@@ -69,21 +88,38 @@ def load_data(path):
     return data
 
 
+def calculate_accuracy(y, t):
+
+    return 100 - (np.mean(np.abs(y - t))*100)
+    
+
+def calculate_fscore(y, t):
+    
+    epsilon = 1e-20
+
+    tp = np.sum(t*y) + epsilon
+    pp = np.sum(y) + epsilon
+    ap = np.sum(t) + epsilon
+
+    precision = tp/pp
+    recall = tp/ap
+    
+    fscore = (2*precision*recall)/(precision + recall + epsilon)
+    return fscore
+
+
+
 if __name__ == '__main__':
 
     path = 'data/data.npy'
-    # path = 'data.npy'
     data = load_data(path)
 
     # from here each sample is a row
     X, t = segregate_target(data)
-    data = train_test_validation_split(X, t, test_ratio=0.30)
+    X = normalize(X)
+    data = train_test_validation_split(X, t, test_ratio=0.15)
 
-    data['X_train'] = normalize(data['X_train'])
-    data['X_valid'] = normalize(data['X_valid'])
-    data['X_test'] = normalize(data['X_test'])
-
-    # from here each sample is a column
+    # stacking each sample as a column
     X_train = data['X_train'].T
     t_train = data['t_train'].reshape(1,-1)
     X_test = data['X_test'].T
@@ -91,38 +127,40 @@ if __name__ == '__main__':
     X_val = data['X_valid'].T
     t_val = data['t_valid'].reshape(1, -1)
 
-    # print(X_train.shape)
-    # print(X_train)
-    L_ = 3
-    n_ = [10, 8, 5, 1]
-    activation_ = 'relu'
-    eta_ = 0.00015
-    model = Model(L_, n_, activation_='relu', learning_rate_=eta_, max_iters_=1000)
+    n_ = [10, 7, 6, 1]
+    activations_ = [None, 'relu', 'relu', 'sigmoid']
+    eta_ = 0.0001
+    model = Model(n_, activations_, learning_rate_=eta_, max_iters_=5000)
+    costs, preds = model.fit(X_train, t_train)
+    print('Train size: ', X_train.shape[1])
+    print('Training accuracy is: ', calculate_accuracy(preds, t_train))
+    print('Training fscore is: ', calculate_fscore(preds, t_train))
+    print()
 
-    print(model.fit(X_train, t_train))
+    preds = model.predict(X_val)
+    print('Validation size: ', X_val.shape[1])
+    print('Validation accuracy is: ', calculate_accuracy(preds, t_val))
+    print('Validation fscore is: ', calculate_fscore(preds, t_val))
+    print()
 
     preds = model.predict(X_test)
-    print(model.calculate_accuracy(preds, t_test))
+    print('Test size: ', X_test.shape[1])
+    print('Testing accuracy is: ', calculate_accuracy(preds, t_test))
+    print('Testing fscore is: ', calculate_fscore(preds, t_test))
+    print()
 
-    # print(model.w)
+    # plt.xscale(value='log')
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    # plt.suptitle('Learning Rate=0.005, Regularization=None')
+    # plt.plot(regs, trains)
+    plt.plot(costs)
+    plt.show()
 
     """
-    Train > Test
-    ------------
-    L_ = 3
-    n_ = [10, 15, 6, 1]
-    activation_ = 'relu'
-    eta_ = 0.00001
-
-    L_ = 3
-    n_ = [10, 8, 4, 1]
-    activation_ = 'relu'
-    eta_ = 0.00001
-
-    Do test on X_test with the following
-    L_ = 3
-    n_ = [10, 15, 4, 1]
-    activation_ = 'relu'
-    eta_ = 0.00005
-    model = Model(L_, n_, activation_='relu', learning_rate_=eta_, max_iters_=2000)
+    np.random.seed(3)
+    n_ = [10, 7, 6, 1]
+    activations_ = [None, 'relu', 'relu', 'sigmoid']
+    eta_ = 0.0001
+    model = Model(n_, activations_, learning_rate_=eta_, max_iters_=3000)
     """

neural-networks/neural_network.py
@@ -1,9 +1,9 @@
 import numpy as np
-np.set_printoptions(precision=4, linewidth=100, suppress=True)
+np.random.seed(3)
 
 class Model:
     
-    def __init__(self, L_, n_, w_={}, b_={}, activation_='relu', learning_rate_=0.0001, max_iters_=1000):
+    def __init__(self, n_, activations_, w_={}, b_={}, learning_rate_=0.0001, max_iters_=1000):
         
         '''
             A Model is a deep neural network whose configuration is contained in `n`. Its paramters are
@@ -11,31 +11,29 @@ class Model:
             'relu' or 'sigmoid'. The output layer always uses 'sigmoid' activation.
             
             Arguments:
-                L_:             number of layers in neural network (excluding input layer)
                 n_:             neural network configuration, specifically `n_[i]` is the number of nodes in layer i
                 w_:             weight parameters for each layer, specifically `w_[i]` is the weight matrix of layer i
                 b_:             biases for each layer, specifically `b_[i]` is the bias vector for layer i
-                activation_:    activation for each hidden layer
+                activations_:   activations for each hidden layer. `activations[0]` SHOULD be None
                 learning_rate_: learning rate for updation of weights
                 max_iters_:     maximum number of iterations during training
         '''
         
-        self.L = L_
         self.n = n_
+        self.L = len(self.n) - 1
+        self.activations = activations_
         self.w = w_
         self.b = b_
-        self.activation = activation_
         self.learning_rate = learning_rate_
         self.max_iters = max_iters_
-
         self.initialize_parameters()
         
     
     def initialize_parameters(self):
     
-        # the ith hidden layer should be of shape (n[i - 1], n[i])
+        # the ith hidden layer should be of shape (n[i], n[i - 1])
         for i in range(1, self.L + 1):
-            self.w[i] = np.random.randn(self.n[i - 1], self.n[i])
+            self.w[i] = np.random.randn(self.n[i], self.n[i - 1])
             self.b[i] = np.random.randn(self.n[i], 1)
             
             
@@ -44,37 +42,54 @@ class Model:
         x = np.copy(a)
         x[x < -15] = -15
         x[x > 15] = 15
-
         return 1/(1 + np.exp(-x))
     
 
     def relu(self, a):
 
         x = np.copy(a)
-        x[x < 0] = 0
-
+        x[x < 0] = 0.0
         return x
 
 
-    def activation_function(self, a):
+    def tanh(self, a):
 
-        if self.activation == 'relu':
+        x = 2*np.copy(a)
+        x[x < -15] = -15
+        x[x > 15] = 15
+        return (np.exp(x) - 1)/(np.exp(x) + 1)
+
+
+    def activation_function(self, a, layer):
+
+        '''
+            Computes the activation for ith layer. i >= 1.
+            i = 0 corresponds to input layer and has no activation
+        '''
+
+        if self.activations[layer] == 'relu':
             return self.relu(a)
 
-        if self.activation == 'sigmoid':
+        if self.activations[layer] == 'sigmoid':
             return self.sigmoid(a)
+
+        if self.activations[layer] == 'tanh':
+            return self.tanh(a)
         
 
-    def derivative(self, a):
+    def derivative(self, a, layer):
 
-        if self.activation == 'relu':
+        if self.activations[layer] == 'relu':
             x = np.copy(a)
-            x[x > 0] = 1
-            x[x < 0] = 0
+            x[x >= 0] = 1.0
+            x[x < 0] = 0.0
             return x
 
-        if self.activation == 'sigmoid':
+        if self.activations[layer] == 'sigmoid':
             return self.sigmoid(a)*self.sigmoid(1 - a)
+
+        if self.activations[layer] == 'tanh':
+            return 1 - (self.tanh(a)**2)
         
     
     def forward_propagate(self, X):
@@ -83,12 +98,9 @@ class Model:
         a = {}
         z[0] = X
         M = X.shape[1]
-        for i in range(1, self.L):
-            a[i] = np.matmul(self.w[i].T, z[i - 1]).reshape(self.n[i], M) + self.b[i]
-            z[i] = self.activation_function(a[i])
-        a[self.L] = np.matmul(self.w[self.L].T, z[self.L - 1]).reshape(self.n[self.L], M) + self.b[self.L]
-        z[self.L] = self.sigmoid(a[self.L])
-
+        for i in range(1, self.L + 1):
+            a[i] = np.matmul(self.w[i], z[i - 1]) + self.b[i]
+            z[i] = self.activation_function(a[i], i)
         return a, z
     
     
@@ -99,13 +111,13 @@ class Model:
         db = {}
 
         delta[self.L] = z[self.L] - t
-        dw[self.L] = np.matmul(z[self.L - 1], delta[self.L].T)
+        dw[self.L] = np.matmul(delta[self.L], z[self.L - 1].T)
         db[self.L] = np.sum(delta[self.L], axis=1, keepdims=True)
 
         for i in range(self.L - 1, 0, -1):
 
-            delta[i] = self.derivative(a[i])*np.matmul(self.w[i + 1], delta[i + 1])
-            dw[i] = np.matmul(z[i - 1], delta[i].T)
+            delta[i] = self.derivative(a[i], i)*np.matmul(self.w[i + 1].T, delta[i + 1])
+            dw[i] = np.matmul(delta[i], z[i - 1].T)
             db[i] = np.sum(delta[i], axis=1, keepdims=True)
 
         return dw, db
@@ -123,26 +135,28 @@ class Model:
     
         a, z = self.forward_propagate(X)
         preds = np.copy(z[self.L])
-        preds[preds > 0.5] = 1
+        preds[preds >= 0.5] = 1
         preds[preds < 0.5] = 0
-
         return preds
 
+    def compute_cost(self, y, t):
 
-    def calculate_accuracy(self, y, t):
-    
-        return 100 - (np.mean(np.abs(y - t))*100)
+        y = np.copy(y)
+        y[y == 1] = 1 - 1e-20
+        y[y == 0] = 1e-20
+        return -np.sum(t*np.log(y) + (1 - t)*(np.log(1 - y)))
 
     
     def fit(self, X, t):
         
+        costs = np.zeros(self.max_iters)
         for i in range(self.max_iters):
             
             a, z = self.forward_propagate(X)
+            costs[i] = self.compute_cost(z[self.L], t)
             dw, db = self.back_propagate(a, z, t)
             self.update(dw, db)
                         
         preds = self.predict(X)
-        training_accuracy = self.calculate_accuracy(preds, t)
         
-        return training_accuracy
+        return costs, preds