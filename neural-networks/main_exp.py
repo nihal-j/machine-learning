Neural Network: Fix normalization and initialization

neural-networks/main_exp.py
@@ -1,7 +1,9 @@
 from neural_network import Model
-from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+
+np.random.seed(42)
 
 def normalize(data, method='min-max'):
     
@@ -114,7 +116,8 @@ if __name__ == '__main__':
 
     # from here each sample is a row
     X, t = segregate_target(data)
-    X = normalize(X)
+    X = normalize(X, method='standardization')
+    # X = normalize(X)
     data = train_test_validation_split(X, t, test_ratio=0.15)
 
     # stacking each sample as a column
@@ -125,11 +128,14 @@ if __name__ == '__main__':
     X_val = data['X_valid'].T
     t_val = data['t_valid'].reshape(1, -1)
 
-    n_ = [10, 7, 6, 1]
-    activations_ = [None, 'relu', 'relu', 'sigmoid']
+    n_ = [10, 18, 1]
+    activations_ = [None, 'relu', 'sigmoid']
     eta_ = 0.0001
+
     model = Model(n_, activations_, learning_rate_=eta_, max_iters_=5000)
     costs, preds = model.fit(X_train, t_train)
+    print(preds)
+    print(t_train)
     print('Train size: ', X_train.shape[1])
     print('Training accuracy is: ', calculate_accuracy(preds, t_train))
     print('Training fscore is: ', calculate_fscore(preds, t_train))
@@ -150,7 +156,7 @@ if __name__ == '__main__':
     # plt.xscale(value='log')
     plt.xlabel('Iterations')
     plt.ylabel('Loss')
-    # plt.suptitle('Learning Rate=0.005, Regularization=None')
+    # plt.suptitle('Learning Rate=0.0001, Number of iterations=3000')
     # plt.plot(regs, trains)
     plt.plot(costs)
     plt.show()
@@ -161,4 +167,10 @@ if __name__ == '__main__':
     activations_ = [None, 'relu', 'relu', 'sigmoid']
     eta_ = 0.0001
     model = Model(n_, activations_, learning_rate_=eta_, max_iters_=3000)
+
+    Exploding_gradients:
+    n_ = [10, 25, 1]
+    activations_ = [None, 'relu','sigmoid']
+    eta_ = 0.001
+    model = Model(n_, activations_, learning_rate_=eta_, max_iters_=10000)
     """

neural-networks/neural_network.py
@@ -1,5 +1,4 @@
 import numpy as np
-np.random.seed(3)
 
 class Model:
     
@@ -33,8 +32,10 @@ class Model:
     
         # the ith hidden layer should be of shape (n[i], n[i - 1])
         for i in range(1, self.L + 1):
-            self.w[i] = np.random.randn(self.n[i], self.n[i - 1])
-            self.b[i] = np.random.randn(self.n[i], 1)
+            self.w[i] = np.random.randn(self.n[i], self.n[i - 1])/np.sqrt(self.n[i])
+            # self.w[i] = np.random.randn(self.n[i], self.n[i - 1])*0.01
+            # self.b[i] = np.random.randn(self.n[i], 1)
+            self.b[i] = np.zeros((self.n[i], 1))
             
             
     def sigmoid(self, a):
@@ -81,8 +82,8 @@ class Model:
 
         if self.activations[layer] == 'relu':
             x = np.copy(a)
-            x[x >= 0] = 1.0
-            x[x < 0] = 0.0
+            x[x > 0] = 1.0
+            x[x <= 0] = 0.0
             return x
 
         if self.activations[layer] == 'sigmoid':
@@ -135,8 +136,8 @@ class Model:
     
         a, z = self.forward_propagate(X)
         preds = np.copy(z[self.L])
-        preds[preds >= 0.5] = 1
-        preds[preds < 0.5] = 0
+        preds[preds > 0.5] = 1
+        preds[preds <= 0.5] = 0
         return preds
 
     def compute_cost(self, y, t):
@@ -149,11 +150,23 @@ class Model:
     
     def fit(self, X, t):
         
+        print_costs = True
+        print_step = 1000
+        learning_rate_updated = False
         costs = np.zeros(self.max_iters)
         for i in range(self.max_iters):
             
             a, z = self.forward_propagate(X)
             costs[i] = self.compute_cost(z[self.L], t)
+
+            if print_costs and i % print_step == 0:
+                print('Cost after iteration ', i, ': ', costs[i])
+
+            # if i > 0 and (costs[i] - costs[i - 1])/costs[i - 1] < 0.01 and learning_rate_updated == False:
+            if i > self.max_iters/2 and learning_rate_updated == False:
+                self.learning_rate *= 0.1
+                learning_rate_updated = True
+
             dw, db = self.back_propagate(a, z, t)
             self.update(dw, db)
                         